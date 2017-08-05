89c4088c9cbd74a5dbbdb44478e4e519c7f882c5
wangruoxuan <ruoxuanwanggoing@gmail.com>
2017-08-05 15:33:49 +0800

    feat: clean up redundant code

---
+++ bin/CMakeFiles/source_lib.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for source_lib.
+# This may be replaced when dependencies are built.
--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/wangruoxuan/ClionProjects/complier
-# Build directory: /Users/wangruoxuan/ClionProjects/complier/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(test)
